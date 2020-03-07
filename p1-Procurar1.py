7c98fc60d8ff32b8ead44a5b25d7e554c268b26a
David Pinto <daxtery11@gmail.com>
2020-03-07 11:30:12 +0000

    New way of doing search. More object-oriented. Less messy :d

--- p1-Procurar1.py
+++ p1-Procurar1.py
@@ -1,203 +1,110 @@
-nome = (input('nome do ficheiro(com extensão)'))    #pergunta o nome do ficheiro e guarda em nome
-s = open(nome)              # abre o ficheiro para poder ler
-linhas = int(s.readline())     # lê o numero de linhas e transforma em inteiro
-palavras = list()           #cria nova lista vazia
-sopal = list()               #cria nova lista vazia
-for i in range(linhas):         #percorre as x linhas seguintes e coloca na lista
-    palavras.append((s.readline().rstrip().upper()))
-line = s.readline()
-while line!='':     #percorre o ficheiro até ao fim e adiciona á matriz sopa
-    sopal.append((line.rstrip()))
-    line = s.readline()
-####################################################################################################
-#este- Esta função percorre cada linha da sopa de letras da esquerda para a direita
-#
-#Argumentos:
-#palavra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrada a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-####################################################################################################
-def este(palavra,inicial,sopa):
-    index = 1
-    while index < len(palavra) and index + inicial[1] < len(sopa[inicial[0]]):
-        if palavra[index]!=sopa[inicial[0]][index + inicial[1]]:
-            break
-        if index == (len(palavra)-1):
-            if palavra[index]==sopa[inicial[0]][index + inicial[1]]:
-                print('%s : %d%s-%d%s, este' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]+1,chr(97+inicial[1]+index)))
-                return True
-        index += 1
-####################################################################################################
-#oeste- Esta função percorre cada linha da sopa de letras da direita para a esquerda
-#
-#Argumentos:
-#palavra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrada a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-####################################################################################################
-def oeste(palavra,inicial,sopa):
-    index = 1
-    while (inicial[1]-index >= 0 and index<len(palavra)):
-        if(palavra[index]!=sopa[inicial[0]][inicial[1]-index]):
-            break
-        if index == (len(palavra)-1):
-            if palavra[index]==sopa[inicial[0]][-index + inicial[1]]:
-                print('%s : %d%s-%d%s, oeste' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]+1,chr(97+inicial[1]-index)))
-                return True
-        index+=1
-####################################################################################################
-#sul- Esta função percorre cada linha da sopa de letras de cima para baixo
-#
-#Argumentos:
-#palvra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrado a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-####################################################################################################
-def sul(palavra,inicial,sopa):
-    index = 1
-    while (index + inicial[0] < len (sopa) and index<len(palavra)):
-        if(palavra[index]!=sopa[inicial[0]+index][inicial[1]]):
-            break
-        if index == (len(palavra)-1):
-            if palavra[index]==sopa[inicial[0]+index][inicial[1]]:
-                print('%s : %d%s-%d%s, sul' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]+index+1,chr(97+inicial[1])))
-                return True
-        index+=1
-####################################################################################################
-#norte- Esta função percorre cada linha da sopa de letras de baixo para cima
-#
-#Argumentos:
-#palvra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrado a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-####################################################################################################
-def norte(palavra,inicial,sopa):
-    index = 1
-    while (inicial[0]-index >= 0 and index<len(palavra)):
-        if(palavra[index]!=sopa[inicial[0]-index][inicial[1]]):
-            break
-        if index == (len(palavra)-1):
-            if palavra[index]==sopa[inicial[0]-index][inicial[1]]:
-                print('%s : %d%s-%d%s, norte' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]-index+1,chr(97+inicial[1])))
-                return True
-        index+=1
-############################################################################################################
-#sudeste- Esta função percorre cada linha da sopa de letras da esquerda para a direita e de cima para baixo
-#
-#Argumentos:
-#palvra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrado a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-############################################################################################################
-def sudeste(palavra,inicial,sopa):
-    index = 1
-    while ((index + inicial[0] < len (sopa))and (index + inicial[1] < len(sopa[inicial[0]])) and (index<len(palavra))):
-        if(palavra[index]!=sopa[inicial[0]+index][inicial[1]+index]):
-            break
-        if index == (len(palavra)-1):
-            if palavra[index]==sopa[inicial[0]+index][inicial[1]+index]:
-                print('%s : %d%s-%d%s, sudeste' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]+index+1,chr(97+inicial[1]+index)))
-                return True
-        index+=1
-############################################################################################################
-#sudoeste- Esta função percorre cada linha da sopa de letras da direita para a esquerda e de cima para baixo
-#
-#Argumentos:
-#palvra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrado a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-############################################################################################################
-def sudoeste(palavra,inicial,sopa):
-    index = 1
-    while ((index + inicial[0] < len (sopa)) and (inicial[1]-index >= 0) and (index<len(palavra))):
-        if(palavra[index]!=sopa[inicial[0]+index][inicial[1]-index]):
-            break
-        if index == (len(palavra)-1):
-            if palavra[index]==sopa[inicial[0]+index][inicial[1]-index]:
-                print('%s : %d%s-%d%s, sudoeste' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]+index+1,chr(97+inicial[1]-index)))
-                return True
-        index+=1
-############################################################################################################
-#noroeste- Esta função percorre cada linha da sopa de letras da esquerda para a direita e de baixo para cima
-#
-#Argumentos:
-#palvra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrado a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-############################################################################################################
-def noroeste(palavra,inicial,sopa):
-    index = 1
-    while ((inicial[1]-index >= 0) and (index < len(palavra)) and ((inicial[0]-index >= 0))):
-        if(palavra[index]!=sopa[inicial[0]-index][inicial[1]-index]):
-            break
-        index+=1
-    print('%s : %d%s-%d%s, noroeste' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]-len(palavra)+1,chr(97+inicial[1]-index)))
-    return True
-############################################################################################################
-#nordeste- Esta função percorre cada linha da sopa de letras da direita para a esquerda e de baixo para cima
-#
-#Argumentos:
-#palvra- Lista com cada palavra a procurar na sopa de letras
-#inicial- Tuplo onde foi encontrado a primeira letra da palavra
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra, direção em que a palavra está na sopa de letras
-############################################################################################################
-def nordeste(palavra,inicial,sopa):
-    index = 1
-    while ((index + inicial[1] < len(sopa[inicial[0]])) and (index < len(palavra)) and ((inicial[0]-index >= 0))):
-        if(palavra[index]!=sopa[inicial[0]-index][inicial[1]+index]):
-            break
-        if index == (len(palavra)-1):
-            if palavra[index]==sopa[inicial[0]-index][inicial[1]+index]:
-                print('%s : %d%s-%d%s, nordeste' % (palavra.lower(),inicial[0]+1,chr(97+inicial[1]),inicial[0]-index+1,chr(97+inicial[1]+index)))
-                return True
-        index+=1
-##########################################################################################################################
-#localizar-Esta função imprime no ecrã o valor de retorno das funções anteriores
-#
-#Argumentos:
-#palvra- Lista com cada palavra a procurar na sopa de letras
-#sopa-Lista com cada linha da sopa de letras
-#Valor de retorno:
-#coordenada da primeira e última letra da palavra e direção em que a palavra está na sopa de letras de todas as palavras
-##########################################################################################################################
-def localizar(palavras,sopa):
-    for palavra in palavras:
-        ini = palavra[0]
-        for linha in range(len(sopa)):
-            for letra in range(len(sopa[linha])):
-                if sopa[linha][letra] == ini:
-                    inicial = (linha,letra)
-                    if este(palavra,inicial,sopa):
-                        break
-                    elif oeste(palavra,inicial,sopa):
-                        break
-                    elif sul(palavra,inicial,sopa):
-                        break
-                    elif norte(palavra,inicial,sopa):
-                        break
-                    elif sudeste(palavra,inicial,sopa):
-                        break
-                    elif sudoeste(palavra,inicial,sopa):
-                        break
-                    elif noroeste(palavra,inicial,sopa):
-                        break
-                    elif nordeste(palavra,inicial,sopa):
-                        break
-localizar(palavras,sopal)
-s.close()
+from typing import List, Tuple, Optional
+from enum import Enum
+
+from puzzle import Puzzle
+
+
+def words_looking_for(path: str) -> List[str]:
+    with open(path, 'r') as file:
+        number_of_lines = int(file.readline())
+        words = []
+        for i in range(number_of_lines):
+            words.append(file.readline().rstrip().upper())
+        return words
+
+
+class Orientation(Enum):
+    Horizontal = 1
+    Vertical = 2
+    Diagonal = 3
+    AntiDiagonal = 4
+
+    def get_vector(self) -> Tuple[int, int]:
+        if self.name == self.Vertical.name:
+            return 1, 0
+        elif self.name == self.Horizontal.name:
+            return 0, 1
+        elif self.name == self.Diagonal.name:
+            return 1, 1
+        else:
+            return -1, 1
+
+
+class PuzzleWord:
+    def __init__(self, word: str, orientation: Orientation, is_inverted: bool, start: Tuple[int, int], end: Tuple[int, int]):
+        self.word = word
+        self.is_inverted = is_inverted
+        self.end = end
+        self.start = start
+        self.orientation = orientation
+
+    def __str__(self):
+        inverted = "(Inverted)" if self.is_inverted else ""
+        return f"{self.word} {self.start} -[{self.orientation}{inverted}]-> {self.end}"
+
+    def __repr__(self):
+        return str(self)
+
+
+class PuzzleWordsLookup:
+    def __init__(self, words: List[str], puzzle: Puzzle):
+        self.words = words
+        self.puzzle = puzzle
+
+    def resolve(self) -> List[PuzzleWord]:
+        words_in_puzzle = []
+        for word in self.words:
+            words_in_puzzle.append(self.look_for_word(word))
+        return words_in_puzzle
+
+    def look_for_word(self, word: str) -> Optional[PuzzleWord]:
+        for row_index in range(len(self.puzzle.rows)):
+            for letter_index in range(len(self.puzzle.rows[row_index])):
+                if self.puzzle.get(row_index, letter_index) == word[0]:
+                    for orientation in Orientation:
+                        # look in the "normal way": for example:
+                        # looking in Horizontal in the "normal" way means
+                        # we look -->
+                        found = self.look_for_word_with_orientation(word, (row_index, letter_index), orientation, False)
+                        if found is not None:
+                            return PuzzleWord(word, orientation, False, (row_index, letter_index), found)
+
+                        # look in the "inverted way"
+                        # while in the "inverted way"
+                        # we look <--
+                        found = self.look_for_word_with_orientation(word, (row_index, letter_index), orientation, True)
+                        if found is not None:
+                            return PuzzleWord(word, orientation, True, (row_index, letter_index), found)
+
+        # fuck
+        print(f"Could not find '{word}'")
+        return None
+
+    def look_for_word_with_orientation(self, word: str, start: Tuple[int, int], orientation: Orientation,
+                                       inverted: bool) -> Optional[Tuple[int, int]]:
+        change_vector = orientation.get_vector()
+        if inverted:
+            change_vector = -change_vector[0], -change_vector[1]
+
+        current_index = 1
+        current_position = 0, 0
+
+        # print("\n\n")
+        # print("start", start, "word", word, "orientation", orientation, "inverted", inverted)
+        # print("\n\n")
+
+        while current_index < len(word):
+            current_position = start[0] + current_index * change_vector[0], start[1] + current_index * change_vector[1]
+
+            if not self.puzzle.in_bounds(current_position[0], current_position[1]):
+                return None
+
+            car = self.puzzle.get(current_position[0], current_position[1])
+            # print(current_position, "character", car, "index", current_index, "word[index]", word[current_index])
+
+            if car != word[current_index]:
+                return None
+
+            current_index += 1
+
+        return current_position
