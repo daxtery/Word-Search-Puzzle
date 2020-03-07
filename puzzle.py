e5d9555474406f71f922a91b11fb95712069dd2b
David Pinto <daxtery11@gmail.com>
2020-03-07 16:18:14 +0000

    puzzle's rows are back to being List of List of str. New set function to set a character

--- puzzle.py
+++ puzzle.py
@@ -2,11 +2,14 @@ from typing import List
 
 
 class Puzzle:
-    def __init__(self, rows: List[str]):
+    def __init__(self, rows: List[List[str]]):
         self.rows = rows
 
     def get(self, row: int, column: int) -> str:
         return self.rows[row][column]
+
+    def set(self, character: str, row: int, column: int):
+        self.rows[row][column] = character
 
     def in_bounds(self, row: int, column: int) -> bool:
         return 0 <= row < len(self.rows) and 0 <= column < len(self.rows[row])
