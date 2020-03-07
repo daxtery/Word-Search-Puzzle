7292a21a670563a63a16969bbfee67f6be8e65a6
David Pinto <daxtery11@gmail.com>
2020-03-07 11:17:42 +0000

    Added a new puzzle class

---
+++ puzzle.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Puzzle:
+    def __init__(self, rows: List[List[str]]):
+        self.rows = rows
+
+    def get(self, row: int, column: int) -> str:
+        return self.rows[row][column]
+
+    def in_bounds(self, row: int, column: int) -> bool:
+        return 0 < row < len(self.rows) and 0 < column < len(self.rows[0])
