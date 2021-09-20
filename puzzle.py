f32a4e5ae309e6408e09693593d379ab367ce81a
David Pinto <daxtery11@gmail.com>
2021-09-20 14:12:01 +0100

    remove unused in Puzzle

--- puzzle.py
+++ puzzle.py
@@ -16,15 +16,9 @@ class Puzzle:
     def __getitem__(self, key: Cell):
         return self.letters[key[0]][key[1]]
 
-    def __setitem__(self, key: Cell, value: str):
-        self.letters[key[0]][key[1]] = value
-
     def in_bounds(self, cell: Cell):
         row, column = cell
         return 0 <= row < self.rows and 0 <= column < self.columns
-
-    def __copy__(self):
-        return Puzzle(self.rows, self.columns, self.letters.copy())
 
     def __repr__(self):
         parts = []
