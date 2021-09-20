95a974e2d645cd43e2950333b1c4df288c9bc8f3
David Pinto <daxtery11@gmail.com>
2021-09-20 14:10:15 +0100

    redo generate a bit (no more copies!)

--- generate.py
+++ generate.py
@@ -1,10 +1,13 @@
 import random
 import string
 import sys
-from typing import List, Optional, Dict, Tuple
+from typing import DefaultDict, List, Optional, Dict, Tuple
 
 from puzzle import Puzzle, Soup
-from orientation import Orientation, inverted_orientation_vector
+from orientation import (
+    Orientation,
+    generate_cells_for_word,
+)
 
 from dataclasses import dataclass
 
@@ -39,137 +42,102 @@ class PuzzleConstructorFromConfig:
 
     def construct(self):
 
-        letters: Soup = []
-        for i in range(config.rows):
-            row = []
-            for j in range(config.columns):
-                row.append(" ")
-            letters.append(row)
-
-        puzzle_with_words = self._construct_from_config_helper(
-            Puzzle(config.rows, config.columns, letters), 0
-        )
+        letters = [[" "] * self.config.columns for _ in range(self.config.rows)]
 
         # We were unable to fit all the words there
-        if puzzle_with_words is None:
-            return puzzle_with_words
+        if not self._construct_from_config_helper(letters, 0):
+            return None
 
-        for i in range(len(puzzle_with_words.letters)):
-            for j in range(config.columns):
-                if puzzle_with_words[(i, j)] == " ":
-                    puzzle_with_words[i, j] = random.choice(string.ascii_uppercase)
+        for i in range(self.config.rows):
+            for j in range(self.config.columns):
+                if letters[i][j] == " ":
+                    letters[i][j] = random.choice(string.ascii_uppercase)
 
-        return puzzle_with_words
+        return Puzzle(config.rows, config.columns, letters)
 
-    def _construct_from_config_helper(
-        self, puzzle: Puzzle, index: int
-    ) -> Optional[Puzzle]:
+    def _in_bounds(self, row: int, collumn: int) -> bool:
+        return 0 <= row < self.config.rows and 0 <= collumn < self.config.columns
+
+    def _construct_from_config_helper(self, letters: Soup, index: int):
         # base case; we've added all the words we wanted to, yay!
         if index == len(config.words):
-            return puzzle
+            return True
 
         word = config.words[index]
-        # print("Adding", word)
-        possible_for_this_word = self._possible_points_with_orientation(
-            word, puzzle, config
-        )
+        possible_for_this_word = self._possible_points_with_orientation(word, letters)
+
         while len(possible_for_this_word) > 0:
 
             random_start_position = random.choice(list(possible_for_this_word.keys()))
-            orientations_and_inverted = possible_for_this_word.get(
+            orientations_and_inverted = possible_for_this_word.pop(
                 random_start_position
             )
-            del possible_for_this_word[random_start_position]
 
             for orientation, inverted in orientations_and_inverted:
-                new_puzzle = self._add_word(
-                    word, puzzle, random_start_position, orientation, inverted
-                )
 
-                # print("Added", word, "Puzzle\n", new_puzzle)
+                previous_letters = [" "] * len(word)
 
-                final_puzzle = self._construct_from_config_helper(new_puzzle, index + 1)
-                # print("End", word, "Puzzle\n", final_puzzle)
-
-                if final_puzzle is not None:
-                    return final_puzzle
-
-        # There's nowhere to place this :( We have failed.
-        return None
-
-    def _possible_points_with_orientation(
-        self, word: str, puzzle: Puzzle, config: PuzzleGeneratorConfig
-    ) -> Dict[Tuple[int, int], List[Tuple[Orientation, bool]]]:
-        start_points = {}
-        for row_index in range(len(puzzle.letters)):
-            for letter_index in range(len(puzzle.letters[row_index])):
-                # Sample here gets a random order of the list
-                for orientation in random.sample(
-                    config.orientations, len(config.orientations)
+                for (i, (row, column)) in generate_cells_for_word(
+                    random_start_position, orientation, inverted, word
                 ):
-                    # Sample here gets a random order of the list
+                    previous_letters[i] = letters[row][column]
+                    letters[row][column] = word[i]
+
+                if self._construct_from_config_helper(letters, index + 1):
+                    return True
+
+                for (i, (row, column)) in generate_cells_for_word(
+                    random_start_position, orientation, inverted, word
+                ):
+                    letters[row][column] = previous_letters[i]
+
+        return False
+
+    def _possible_points_with_orientation(self, word: str, letters: Soup):
+        start_points: Dict[
+            Tuple[int, int], List[Tuple[Orientation, bool]]
+        ] = DefaultDict(list)
+
+        for row_index in range(self.config.rows):
+            for letter_index in range(self.config.columns):
+
+                cell = (row_index, letter_index)
+
+                for orientation in random.sample(
+                    self.config.orientations, len(self.config.orientations)
+                ):
                     inverted_list = random.sample([True, False], 2)
                     for inverted in inverted_list:
                         if self._possible_with_location_and_orientation(
                             word,
-                            puzzle,
-                            (row_index, letter_index),
+                            letters,
+                            cell,
                             orientation,
                             inverted,
                         ):
-                            start_points.setdefault(
-                                (row_index, letter_index), []
-                            ).append((orientation, inverted))
+                            start_points[cell].append((orientation, inverted))
 
         return start_points
 
     def _possible_with_location_and_orientation(
         self,
         word: str,
-        puzzle: Puzzle,
+        letters: Soup,
         location: Tuple[int, int],
         orientation: Orientation,
         inverted: bool,
     ) -> bool:
 
-        change_vector = (
-            inverted_orientation_vector(orientation) if inverted else orientation.value
-        )
-
-        for i in range(len(word)):
-            current_location = (
-                location[0] + i * change_vector[0],
-                location[1] + i * change_vector[1],
-            )
-            if not puzzle.in_bounds(current_location):
+        for (index, (row, column)) in generate_cells_for_word(
+            location, orientation, inverted, word
+        ):
+            if not self._in_bounds(row, column):
                 return False
-            if puzzle[current_location] != " " and puzzle[current_location] != word[i]:
+
+            if (letter := letters[row][column]) != " " and letter != word[index]:
                 return False
 
         return True
-
-    def _add_word(
-        self,
-        word: str,
-        puzzle: Puzzle,
-        location: Tuple[int, int],
-        orientation: Orientation,
-        inverted: bool,
-    ) -> Puzzle:
-
-        change_vector = (
-            inverted_orientation_vector(orientation) if inverted else orientation.value
-        )
-        new_puzzle = puzzle.__copy__()
-
-        for i in range(len(word)):
-            current_location = (
-                location[0] + i * change_vector[0],
-                location[1] + i * change_vector[1],
-            )
-            puzzle[current_location] = word[i]
-
-        return new_puzzle
 
 
 if __name__ == "__main__":
